fa784115de49c833b69ae3d5f2886cf29b5c9e20
fefofefi <federicorprats95@gmail.com>
2020-01-29 15:14:32 -0300

    Prueba de KIVY

--- KIVY/src/Techtim2-mykv.py
+++ KIVY/src/Techtim2-mykv.py
@@ -8,9 +8,17 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class Caja1(Widget):
-    pass
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btn(self):
+        print("Name: ", self.name.text, "email: ", self.email.text)
+        self.name.text = ""
+        self.email.text = ""
+
 
 class MyApp(App):
     title = "Falsito truco"
