97c01ffd142b9b99b308dc4a133234b703c9c209
Fefofefi <federicorprats95@gmail.com>
2020-02-03 09:40:21 -0300

    Pruebas en Kivy

--- KIVY/src/Techtim4.py
+++ KIVY/src/Techtim4.py
@@ -10,25 +10,31 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang.builder import Builder
 
-class Caja1(Widget):
+class MainWindow(Screen):
+
     cartas = ObjectProperty(None)
     objetivo = ObjectProperty(None)
 
     def btn(self):
-        print("Se juega con", self.cartas.text,"cartas", "y se juega hasta", self.objetivo.text)
-        global ccartas = self.cartas.text
-        global objetivo = self.objetivo.text
+        print("Se juega con", self.cartas.text, "cartas", "y se juega hasta", self.objetivo.text)
+        cartas = self.cartas.text
+        objetivo = self.objetivo.text
         self.cartas.text = ""
         self.objetivo.text = ""
+        print(cartas, objetivo)
 
+class SecondWindow(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
 
+kv = Builder.load_file("tim4.kv")
 
-
-class MyApp(App):
-    title = "Falsito truco"
+class tim4App(App):
     def build(self):
-        return Caja1()
+        return kv
 
 if __name__ == "__main__":
-    MyApp().run()
+    tim4App().run()
